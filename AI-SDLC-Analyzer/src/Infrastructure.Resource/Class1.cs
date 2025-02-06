ac255dcd655229ec756ba97ef07c899bfa8cf4be
Feroze basha S <bashaferoz66@gmail.com>
2025-02-06 10:55:11 +0530

    Initial

---
+++ AI-SDLC-Analyzer/src/Infrastructure.Resource/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Infrastructure.Resource;
+
+public class Class1
+{
+
+}
---
+++ AI-SDLC-Analyzer/tests/Infrastructure.Data.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Data.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
